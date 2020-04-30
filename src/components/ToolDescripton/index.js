d1c4b6c4d5af65b29806191c0bcf51469c06ef30
MiCetF <webmaster@micetf.fr>
2020-04-30 15:55:32 +0200

    [FEAT] Proposer un code pour embarquer les applications dans un ENT ou autre.

--- src/components/ToolDescripton/index.js
+++ src/components/ToolDescripton/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { connect } from "react-redux";
 
-const ToolDescription = props => {
+const ToolDescription = (props) => {
+    const embed = useRef(null);
+
+    const handleCopy = (e) => {
+        e.preventDefault();
+        console.log(embed.current.textContent);
+        navigator.clipboard.writeText(embed.current.textContent);
+    };
     if (!props.tool) {
         return (
             <h3 className="text-center alert alert-warning">
@@ -10,6 +17,12 @@ const ToolDescription = props => {
             </h3>
         );
     }
+    const embeded = (yrl) => (
+        <code className="clearfix" ref={embed} title="Code d'intégration">
+            &lt;iframe src={`https://micetf.fr/${url}`} width="980" height="735"
+            frameborder="0" &gt;&lt;/iframe&gt;
+        </code>
+    );
     const { title, url, image, description } = props.tool;
     return (
         <div className="col-sm-8">
@@ -19,9 +32,10 @@ const ToolDescription = props => {
                 </div>
                 <div className="card-body">
                     <a
-                        href={`../${url}`}
+                        href={`https://micetf.fr/${url}`}
                         target="_blank"
                         rel="noopener noreferrer"
+                        title="Accéder à l'outil..."
                     >
                         <img
                             src={`./thumbnails/${image}`}
@@ -30,13 +44,36 @@ const ToolDescription = props => {
                         />
                     </a>
                 </div>
-                <div className="card-footer text-justify">{description}</div>
+                <div className="card-footer text-justify">
+                    <div className=" mb-2">
+                        {description} (
+                        <a
+                            href={`https://micetf.fr/${url}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Accéder à l'outil..."
+                        >
+                            Accéder à l'outil...
+                        </a>
+                        )
+                    </div>
+                    <div className="border border-secondary my-1 p-1">
+                        {embeded(url)}
+                        <button
+                            className="mt-1 float-right btn btn-outline-dark"
+                            onClick={handleCopy}
+                            title="Copier le code d'intégration dans le presse-papier."
+                        >
+                            Copy
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     tool: state.selectedTool,
 });
 
